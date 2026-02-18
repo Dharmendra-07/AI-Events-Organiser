ab01661efb835f9f31c53a678aefbaef8c420653
Dharmendra Kumar <justdk06@gmail.com>
2026-02-18 18:41:52 +0530

    update: cursor

--- components/header.jsx
+++ components/header.jsx
@@ -217,7 +217,7 @@ export default function Header() {
               <Button size="sm" asChild className="flex gap-2 mr-4">
                 <Link href="/create-event">
                   <Plus className="w-4 h-4" />
-                  <span className="hidden sm:inline">Create Event</span>
+                  <span className="hidden sm:inline cursor-pointer">Create Event</span>
                 </Link>
               </Button>
 
